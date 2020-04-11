0cffe406e9afb65c0a6f4e9c821f1f7f668cdeed
warwick <warwick.nortier@gmail.com>
2020-04-12 00:43:30 +0200

    api calls exceeded

--- controllers/weather.js
+++ controllers/weather.js
@@ -2,20 +2,15 @@ const WeatherData = require('../models/weather');
 
 
 exports.getLocationKey = async (req, res, next) => {
-    // const user = new User(req.body);
-    // await user.save();
-    // const users = await User.fetchAll();
-    // console.log(users[0])
-    console.log(req.body.location)
-
         const weather = new WeatherData(req.body.location);
         const theKey = await weather.retrieveKey();
         console.log(theKey)
         //const fiveDay = await weather.fiveDayForecast(theKey);
         const currentConditions = await weather.currentConditions(theKey);
+        console.log(currentConditions)
 
     res.status(201).json({
-        message: 'Location Received',
+        message: currentConditions[0].WeatherText
         // details: { users }
     })
 };
--- models/weather.js
+++ models/weather.js
@@ -1,44 +1,49 @@
 const axios = require('axios')
 
-  module.exports = class WeatherData {
-      constructor(location) {
-          this.location = location;
-          this.key = undefined;
-          this.accuWeatherAPIKey = 'Kpz82H3k2hCmDw9TMv78cfqmBkNSdxXS'
-      }
+module.exports = class WeatherData {
+    constructor(location) {
+        this.location = location;
+        this.key = undefined;
+        this.accuWeatherAPIKey = 'Kpz82H3k2hCmDw9TMv78cfqmBkNSdxXS'
+        this.currentConditionsData = undefined;
+    }
 
-      async retrieveKey() {
-          let self = this;
-          const reqString = 'http://dataservice.accuweather.com/locations/v1/cities/search?q=' + this.location + '&apikey=' + this.accuWeatherAPIKey;
-          await axios.get(reqString)
-              .then(function (response) {
-                  self.key = response.data[0].Key
-              })
+    async retrieveKey() {
+        let self = this;
+        const reqString = 'http://dataservice.accuweather.com/locations/v1/cities/search?q=' + this.location + '&apikey=' + this.accuWeatherAPIKey;
+        await axios.get(reqString)
+            .then(function (response) {
+                self.key = response.data[0].Key;
+            })
 
-          return self.key
-      }
+        return self.key
+    }
 
-      async fiveDayForecast(locationKey) {
-          const reqString = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/' + locationKey
-          await axios.get(reqString)
-              .then(function (response) {
-                  console.log(response.data)
-              })
-      }
+    async fiveDayForecast(locationKey) {
+        const reqString = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/' + locationKey
+        await axios.get(reqString)
+            .then(function (response) {
+                //   console.log(response.data)
+            })
+    }
 
-        async currentConditions(locationKey) {
-            try {
-                const reqString = 'https://dataservice.accuweather.com/currentconditions/v1/' + locationKey + '?apikey=' + this.accuWeatherAPIKey + '&details=true'
-                await axios.get(reqString, {
-                        headers: {
-                            'Access-Control-Allow-Origin': '*'
-                        }
-                    })
-                    .then(function (response) {
-                        console.log(response.data)
-                    })
-            } catch (error) {
-                console.log(error)
-            }
+    async currentConditions(locationKey) {
+        try {
+            let self = this;
+            console.log('hello?')
+            const reqString = 'https://dataservice.accuweather.com/currentconditions/v1/' + locationKey + '?apikey=' + this.accuWeatherAPIKey + '&details=true'
+            await axios.get(reqString, {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                })
+                .then(function (response) {
+                    self.currentConditionsData = response.data;
+                })
+
+            return self.currentConditionsData
+        } catch (error) {
+            console.log(error)
         }
-  }
+    }
+}
