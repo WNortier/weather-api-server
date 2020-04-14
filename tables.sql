0818479731550ef5277a2f4aa06ff5b153f490f0
warwick <warwick.nortier@gmail.com>
2020-04-14 10:18:24 +0200

    experimenting with sql files

---
+++ tables.sql
@@ -0,0 +1,5 @@
+show databases;
+use weather-api;
+select * from users;
+insert into users (email, username, password, is_admin) values ('emma@gmail.com', 'emma', 'password', 0);
+select * from users;
